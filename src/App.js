a6493bffc6b6e34157fce3cc15cd42a2a70eef50
Immanuella Busari <im.busari@gmail.com>
2019-08-17 18:21:52 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom'
+
+// import pages
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+
+import Navbar from './components/Navbar'
+
+
 function App() {
-  return (
-    <>Hello from App</>
+  return (<>
+
+  <Navbar />
+  <Switch>
+    <Route exact path='/' component={Home} />
+    <Route exact path='/rooms' component={Rooms} />
+    <Route exact path='/rooms/:id' component={SingleRoom} />
+    <Route component={Error} />
+  </Switch>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Hello from Home Boy!
+        </div>
+    )
+}
+
+export default Home
