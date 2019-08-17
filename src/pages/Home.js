3f6fc35adf9fac9044da145689901282650e7d92
Immanuella Busari <im.busari@gmail.com>
2019-08-17 18:52:40 +0300

    Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>
-            Hello from Home Boy!
-        </div>
+        <Hero>
+            <Banner title='Luxurious Rooms' subtitle='deluxe rooms starting at $299'>
+                <Link to='/rooms' className='btn-primary'> Our rooms</Link>
+            </Banner>
+        </Hero>
     )
 }
 
