0edee71211c87d969d2b71e35bcf7072e135cdd5
Immanuella Busari <im.busari@gmail.com>
2019-08-20 10:55:52 +0300

    Single Room - page [Completed]

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -38,17 +38,60 @@ export default class SingleRoom extends Component {
             </div>
             )
         }
-        const { name, description, capacity, 
-            size, price, extras, breakfast, pets, images } = room
+
+        const { name, description, 
+            capacity, 
+            size, price, 
+            extras, breakfast, 
+            pets, images 
+        } = room
+
+        const [mainImg, ...defaultImg ] = images // Array Destructuring
 
         return (
-            <StyledHero img={images[0] || this.state.defaultBcg}>
+            <>
+            <StyledHero img={mainImg || this.state.defaultBcg}>
                 <Banner title={`${name} room`}>
                     <Link to='/rooms' className='btn-primary'>
                         Back to Rooms
                     </Link>
                 </Banner>
             </StyledHero>
+            <section className='single-room'>
+                <div className='single-room-images'>
+                    {defaultImg.map((item, index) => {
+                        return <img key={index} src={item} alt={name} />
+                    })}
+                </div>
+                <div className='single-room-info'>
+                    <article className='desc'>
+                        <h3>Details</h3>
+                        <p>{ description }</p>
+                    </article>
+                    <article className='info'>
+                        <h3> Info</h3>
+                        <h6> Price : ${price}</h6>
+                        <h6> Size : {size} SQFT</h6>
+                        <h6>
+                            Max Capacity : {
+                                capacity > 1 ? `${capacity} people` : `${capacity} person ` 
+                            }
+                        </h6>
+                        <h6>{pets ? 'pets Allowed' : 'no pets allowed'}</h6>
+                        <h6>{breakfast && 'free breakfast included'}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className='room-extras'>
+                <h6>extras</h6>
+                <ul className='extras'>
+                    {extras.map((item, index) => {
+                        return <li key={index}> - {item}</li>
+                    })}
+                </ul>
+
+            </section>
+            </>
         )
     }
 }
